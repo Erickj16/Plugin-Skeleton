626f8626ffcdc23b1dada81d021db97c8f2fc343
Erick <104103217+Erickj16@users.noreply.github.com>
2022-11-16 10:29:48 -0600

    first commit

---
+++ pasarela-pago/plugin-pasarela.php
@@ -0,0 +1,13 @@
+<?php 
+/**
+ * 
+  Plugin Name:       Plugin-pago
+  Plugin URI:        https://example.com/plugins/Plugin-pago/
+  Description:       prueba de plugin-pago
+  Version:           1.10.3
+  Requires at least: 5.2
+  Requires PHP:      7.2
+  Author:            Erick Icabalzeta
+  Author URI:        https://author.example.com/
+*/
+?>
